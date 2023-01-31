0ca480a6b7427b64ddf8c532d94a06fcf8341083
napoleanBonaparte <106796606+napoleanBonaparte@users.noreply.github.com>
2023-01-31 12:54:42 +0530

    Add files via upload

---
+++ perms.py
@@ -0,0 +1,42 @@
+
+def perms(lst,j):
+
+	for i in range(len(lst)):
+		if lst[i] == j:
+			pass
+		else:
+			temp = lst[i]
+			if lst[i] == lst[len(lst)-1]:
+				lst[i] = lst[0]
+				lst[0] = temp
+			else:
+				lst[i] = lst[i+1]
+				lst[i+1] = temp
+		print(lst)
+
+
+def solve(lst):
+	lst.sort()
+	
+	for i in range(len(lst)):
+		lst.sort()
+		for j in range(len(lst)):
+			copy = lst
+			sj = lst[j]
+			perms(copy,sj)
+			temp = lst[j]
+			if lst[j] == lst[len(lst)-1]:
+				lst[j] = lst[0]
+				lst[0] = temp
+			else:
+				lst[j] = lst[j+1]
+				temp = lst[j+1] 
+
+
+
+solve([1,2,3,4])
+
+
+
+
+
